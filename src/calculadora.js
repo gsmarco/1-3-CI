100fc4330cac4107a4df8dded74e45a4b9601f3f
Marco Antonio González Solis <gsmarcoglez@gmail.com>
2026-09-08 12:19:40 -0600

    Mensaje de error divicion por 0

--- src/calculadora.js
+++ src/calculadora.js
@@ -12,7 +12,7 @@ function multiplicar(a, b) {
 
 function dividir(a, b) {
     if (b === 0) {
-        throw new Error("No se puede dividir entre cero");
+        throw new Error("Error de operación. No se puede dividir entre cero");
     }
 
     return a / b;
